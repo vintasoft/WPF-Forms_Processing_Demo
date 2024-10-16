1de396172cf79d931bbb8f00185f13daff77bcc8
vintasoft <alex@vintasoft.com>
2024-10-16 15:55:46 +0300

    Source codes update

--- CSharp/Properties/AssemblyInfo.cs
+++ CSharp/Properties/AssemblyInfo.cs
@@ -14,9 +14,9 @@ using System.Windows;
 [assembly: AssemblyTitle("VintaSoft WPF Forms Processing Demo")]
 [assembly: AssemblyDescription("Shows how to align the form templates and recognize the forms from an image.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VintaSoft Ltd.")]
+[assembly: AssemblyCompany("VintaSoft Llc.")]
 [assembly: AssemblyProduct("VintaSoft Imaging .NET SDK")]
-[assembly: AssemblyCopyright("Copyright VintaSoft Ltd. 2024")]
+[assembly: AssemblyCopyright("Copyright VintaSoft Llc. 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
